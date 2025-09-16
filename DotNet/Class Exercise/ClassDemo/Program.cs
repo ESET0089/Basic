f9cb16e90b7830fd71e8e4829e155326c5aa9fe0
ESET0089 <ganesh.a@esyasoft.com>
2025-09-16 10:51:02 +0530

    Added DLMS assignment - DLMS Communication demo

---
+++ DotNet/Class Exercise/ClassDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.initialize();
+            student.displayStudentDetails();
+
+            Student firstStudent = new Student(1,"Gourav");
+
+        }
+    }
+}
---
+++ DotNet/Class Exercise/ClassDemo/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    internal class Student
+    {
+        private int student_id;
+        private string name;
+        private int age;
+        private string contactno;
+        private string emailid;
+
+        public void initialize()
+        {
+            student_id = 111;
+            name = "Name";
+        }
+
+        public void displayStudentDetails()
+        {
+            Console.WriteLine("ID", student_id);
+            Console.WriteLine("Name", name);
+        }
+
+        public Student(int student_id, string student_name)
+        {
+            this.student_id = student_id;
+            this.name = student_name;
+        }
+
+        public Student() { }
+    }
+    
+}
