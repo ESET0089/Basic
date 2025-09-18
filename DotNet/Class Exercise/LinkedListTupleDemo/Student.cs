3986764d1c4378683e80bf1e5a4534a2896d22b3
ESET0089 <ganesh.a@esyasoft.com>
2025-09-18 15:31:47 +0530

    Added .Net Exercise for data structures - Stack, Queue, LinkedList, Tuple, Dictionary

---
+++ DotNet/Class Exercise/LinkedListTupleDemo/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Tuple_Demo
+{
+    internal class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }    
+        public int marks { get; set; }
+
+        public Student(int Id, string Name, int Marks) {
+            this.id = Id;
+            this.name = Name;
+            this.marks = Marks;
+        }
+    }
+}
