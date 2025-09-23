5c8a9b453f19e0a2ad3348c071876abde5673041
ESET0089 <ganesh.a@esyasoft.com>
2025-09-23 10:04:50 +0530

    Added demo .NET webapp

---
+++ DotNet/Class Exercise/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private List<string> student = new List<string>
+        {
+            "Raju",
+            "Rashid",
+            "Richard"
+        };
+
+        private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+        {
+            new Dictionary<string, string>{{"id","1"},{"name","Sachin"}},
+            new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+        };
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return new JsonResult(student_dict);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(string id)
+        {
+            var student = student_dict.FirstOrDefault(s => s["id"] == id);
+            if (student != null)
+            {
+                return new JsonResult(student);
+            }
+            return NotFound(new { message = "Student not found" });
+        }
+    }
+}
