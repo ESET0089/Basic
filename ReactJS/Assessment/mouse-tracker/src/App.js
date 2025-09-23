06f0c3c786c7028c2dbfdaed696abf762a64121a
ESET0089 <ganesh.a@esyasoft.com>
2025-09-23 10:28:18 +0530

    Added mouse tracker assignment

---
+++ ReactJS/Assessment/mouse-tracker/src/App.js
@@ -0,0 +1,11 @@
+import MouseTracker from './components/MouseTrackerComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MouseTracker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/Assessment/mouse-tracker/src/components/MouseTrackerComponent.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+const MouseTracker = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    // Add event listener
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className>
+      <h1 className>Mouse Tracker</h1>
+      <p>
+        X: {position.x}, Y: {position.y}
+      </p>
+    </div>
+  );
+};
+
+export default MouseTracker;
